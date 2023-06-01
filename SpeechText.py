6d25d1b4fe6f3306972de9ab5e9edc78ff3fa35c
EddyDomingo <68975969+EddyDomingo@users.noreply.github.com>
2023-06-01 12:21:23 -0600

    Add files via upload

---
+++ SpeechText.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+import os
+import openai
+
+# Set up your OpenAI API key
+openai.api_key = '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'
+
+# # Create a recognizer object
+r = sr.Recognizer()
+
+# # Load the audio file
+audio_file = sr.AudioFile('Prompt.wav')
+
+# # Open the audio file and convert speech to text
+with audio_file as source:
+     audio = r.record(source)
+
+# # Use Google Cloud Speech API for speech recognition
+text = r.recognize_google(audio)
+
+# # Print the transcribed text
+#print(text)
+
+# # Make a request to ChatGPT
+response = openai.Completion.create(
+  engine='text-davinci-003',
+  prompt=text,
+  max_tokens=500,
+  n=2,
+  stop=None,
+  temperature=0.7
+)
+
+# # Get the generated text from the response
+generated_text = response.choices[0].text.strip()
+generated_text1 = response.choices[1].text.strip()
+
+# # Print the generated text
+print(generated_text)
+print('-------------')
+print(generated_text1)
+
+
+
+
+
+
+
+
+
+
